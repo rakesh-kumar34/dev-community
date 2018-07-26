9ca04b78e483e9a7ce1b8a13ddcb672b2f643050
Rakesh Kumar <rakeshgat@gmail.com>
2018-07-26 14:35:53 -0500

    Redux and Client

--- routes/api/users.js
+++ routes/api/users.js
@@ -53,7 +53,7 @@ router.post('/register', (req, res) => {
                 avatar,
                 password: req.body.password
             });
-
+        
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
